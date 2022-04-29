141292c9dc5e7317b9e08c1b6ceec14671bdacea
cutsea110 <cutsea110@gmail.com>
2022-04-29 11:07:26 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,60 @@ use std::{
     fs::File,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::{mpsc, Arc, Mutex},
     thread,
     time::Duration,
 };
 
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+
+        Self { id, thread }
+    }
+}
+
+struct Job;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        Self { workers, sender }
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("bind 127.0.0.1:7878");
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.expect("get stream");
         println!("Connection established!: {:?}", stream);
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
