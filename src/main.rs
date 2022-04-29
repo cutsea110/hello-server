876076b98cb19c8fa9b9c0ef3b2056ce908ef292
cutsea110 <cutsea110@gmail.com>
2022-04-29 09:59:57 +0900

    connect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").expect("bind 127.0.0.1:7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.expect("get stream");
+        println!("Connection established!: {:?}", stream);
+    }
 }
