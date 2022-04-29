142ac86fb56e31f84ce2f637ea72cc6cd31bea4c
cutsea110 <cutsea110@gmail.com>
2022-04-29 10:12:45 +0900

    read stream

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("bind 127.0.0.1:7878");
@@ -6,5 +9,12 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.expect("get stream");
         println!("Connection established!: {:?}", stream);
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).expect("read request to buffer");
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
